9e35438abfabd63bc150012bc1721cb5f24f4a84
peter-matc <2446454333@qq.com>
2023-11-18 19:03:05 +0800

    编辑员工

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -118,4 +118,22 @@ public class EmployeeController {
 
         return Result.success();
     }
+
+
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable("id") Long id) {
+        Employee employee = employeeService.getById(id);
+//        Result<Employee> result = new Result<>();
+        return Result.success(employee);
+    }
+
+    @PutMapping()
+    @ApiOperation("编辑员工信息")
+    public Result update(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("编辑员工信息，{}", employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
+
 }
