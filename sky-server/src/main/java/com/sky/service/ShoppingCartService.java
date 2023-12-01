b927c159bf33a687a1f3c7bff5cd4758ad336b45
peter-matc <2446454333@qq.com>
2023-12-01 11:37:30 +0800

    查看购物车

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,6 +1,9 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 /* *
  * @packing com.sky.service
@@ -15,4 +18,11 @@ public interface ShoppingCartService {
      * @param shoppingCartDTO
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     *
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
 }
